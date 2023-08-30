5e08c2e37dd86089b7f7cdd08660f71679fcbc3b
Ivy Reed <idcfu788@gmail.com>
2023-08-29 23:21:48 -0400

    letter

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../assets/styles/forms.css";
+import "../assets/styles/Forms.css";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
